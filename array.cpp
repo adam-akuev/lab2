a548d5116a47c14a86772b119c523672be557100
adam-akuev <109418017+adam-akuev@users.noreply.github.com>
2024-03-03 16:24:41 +0300

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main() {
+	system("chcp 1251");
+	srand(time(NULL));
+	int a[15];
+	int b[15];
+	int ASD = 0;
+	int SUM = 0;
+	cout << "a[ ";
+	for (int i = 0; i < 15; i++) {
+		*(a + i) = 1 + rand() % 30 - 10;
+	}
+	for (int i = 0; i < 15; i++) {
+		cout << *(a + i) << " ";
+	}
+	cout << "]\n";
+	cout << "b[ ";
+	for (int i = 2; i < 15; i += 3) {
+		*(b + ASD) = *(a + i);
+		ASD++;
+	}
+	for (int i = 0; i < 5; i++) {
+		cout << *(b + i) << " ";
+		SUM++;
+	}
+	cout << "]\n";
+	cout << "Size array b: " << SUM;
+	return 0;
+
+
+}
+	/*srand(time(NULL));
+		int result = 1 + rand() % 20;*/
---
+++ lab2_C++.cpp.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <time.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+	system("chcp 1251");
+	srand(time(NULL));
+	int n;
+	float max = 0;
+	float sum = 0;
+	cout << "Enter the number of elements in the array: ";
+	cin >> n;
+	// 1
+	float* array = new float[n];
+	for (int i = 0; i < n; i++) {
+		*(array + i) = 10.0 - 20.0 * rand() / (float)RAND_MAX;
+	}
+	cout << "A[";
+	for (int i = 0; i < n; i++) {
+		cout << *(array + i) << " ";
+		if (max < *(array + i))
+		max = *(array + i);
+		if (*(array + i) < 0) {
+			float x = fabs(*(array + i));
+			if (max < x)
+				max = x;
+		}
+	}
+	cout << "]\n";
+	cout << "Module max: " << max << "\n";
+	// 2
+	bool flag = false;
+	for (int i = 0; i < n; i++) {
+		if (*(array + i) > 0) {
+			flag = true;
+		}
+		else if (flag) {
+			sum += *(array + i);
+		}
+	}
+	cout << "Sum after positive element: " << sum << endl;
+
+	// 3
+	int a, b;
+	cout << "add a:";
+	cin >> a;
+	cout << "add b:";
+	cin >> b;
+	cout << "A[ ";
+	for (int i = 0; i < n; i++) {
+		if (*(array + i) >= a && *(array + i) <= b) {
+			cout << *(array + i) << " ";
+		}
+	}
+	for (int i = 0; i < n; i++)
+		if (*(array + i) < a || *(array + i) > b)
+			cout << *(array + i) << " ";
+	cout << "]";
+	delete[] array;
+	return 0;
+}
